a4a866a4fec27fc451f8d3c1f1800f0e10d33191
Tesssssssssy <ewoo97@naver.com>
2024-07-15 23:37:25 +0900

    [Fix] 배포 시 쿠키를 이용한 로그인이 되지 않는 문제 해결
    - 원인: :Secure 설정은 HTTPS에서만 쿠키를 주고 받을 수 있는 설정이므로 이 설정 제거

--- src/utils/authCookies.js
+++ src/utils/authCookies.js
@@ -3,8 +3,11 @@ export function setTokenCookies(accessToken, refreshToken) {
     const hour = 3600000; // 밀리초 단위로 1시간
     const week = hour * 24 * 7; // 밀리초 단위로 1주일
 
-    document.cookie = `accessToken=${accessToken}; path=/; max-age=${hour}; Secure; SameSite=Strict`;
-    document.cookie = `refreshToken=${refreshToken}; path=/; max-age=${week}; Secure; SameSite=Strict`;
+    // document.cookie = `accessToken=${accessToken}; path=/; max-age=${hour}; Secure; SameSite=Strict`; // Secure 플래그는 HTTPS에서만 (이것 때문에 배포 후 쿠키 저장 안됨!)
+    // document.cookie = `refreshToken=${refreshToken}; path=/; max-age=${week}; Secure; SameSite=Strict`;
+
+    document.cookie = `accessToken=${accessToken}; path=/; max-age=${hour}; SameSite=Strict`;
+    document.cookie = `refreshToken=${refreshToken}; path=/; max-age=${week}; SameSite=Strict`;
 }
 
 // 쿠키에서 토큰을 추출하는 함수
